a279dddd27b82a6713fbae809423ef42251509d7
Hunter Scholz <scholzh@oregonstate.edu>
2024-10-17 11:40:42 -0700

    unit 2 project done

---
+++ cplorations/c03/main.c
@@ -0,0 +1,13 @@
+/****************************************
+ * C-ploration 3 for CS 271
+ * 
+ * [NAME] Hunter Scholz
+ * [TERM] FALL 2024
+ * 
+ ****************************************/
+
+final int MAX_LINE_LENGTH  200;
+ 
+int main(int argc, const char *argv[]){	
+	return 1;
+}
