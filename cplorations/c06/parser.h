ae3f2dba21b01e2e6f1c8e09ba8753017e63332d
Hunter Scholz <scholzh@oregonstate.edu>
2024-11-05 13:50:07 -0800

    cploration 6

---
+++ cplorations/c06/parser.h
@@ -0,0 +1,59 @@
+/****************************************
+ * C-ploration 6 for CS 271
+ * 
+ * [NAME] Hunter Scholz
+ * [TERM] FALL 2024
+ * 
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MAX_LINE_LENGTH  200
+#define MAX_LABEL_LENGTH  MAX_LINE_LENGTH - 2
+
+/** function prototypes **/
+char *strip(char *s);
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+enum instr_type {
+	Invalid = -1,
+	Atype,
+	Ctype,
+};
+
+typedef struct c_instruction {
+	opcode a:1;
+	opcode comp:6;
+	opcode dest:3;
+	opcode jump:3;
+} c_instruction;
+
+typedef struct a_instruction {
+	union {
+		hack_addr address;
+		char * label;
+	};
+	bool is_addr;
+} a_instruction;
+
+typedef struct instruction {
+	union {
+		a_instruction;
+		c_instruction;
+	};
+	enum instr_type field;
+} instruction;
+
+void parse(FILE * file);
+
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
+
+char *extract_label(const char *line, char* label);
+
