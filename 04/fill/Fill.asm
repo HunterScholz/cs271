6dd314b976f65f712792769d4ffeae17dd261f07
Hunter Scholz <scholzh@oregonstate.edu>
2024-11-07 13:05:16 -0800

    project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,52 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(SETUP)
+    @SCREEN
+    D=A
+    @0
+    M=D
+
+(KEY)
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    @0
+    A=M
+    M=-1
+    @0
+    D=M+1
+    @KBD
+    D=A-D
+    @0
+    M=M+1
+    A=M
+    @EXIT
+    0;JMP
+
+(WHITE)
+    @0
+    A=M
+    M=0
+    @0
+    D=M+1
+    @KBD
+    D=A-D
+    @0
+    M=M+1
+    A=M
+    @EXIT
+    0;JMP
+
+(EXIT)
+    @0
+    D=M
+    @KEY
+    D;JGE
+    @SETUP
+    D;JLT
